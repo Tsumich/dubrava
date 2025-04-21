ecdedce88a4fed8153c5356598ecc574e95961a7
tsumich <lomakinadiana2016@gmail.com>
2025-04-21 19:57:03 +0700

    dfgdfg

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,9 @@ const sequelize = require('./db')
 const models = require('./models/models')
 const router = require('./routes/routes')
 const path = require('path')
- 
+const https = require("https");
+const fs = require("fs");
+
 const app = express()
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
@@ -12,15 +14,22 @@ app.use(cors())
 //app.use(cors({ origin: 'https://dubrava-tb2h.vercel.app' }));
 app.use('/api', router)
 
- 
+const options = {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert"),
+};
 
 const start = async() => {
-    try{
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(9000, () => console.log('Server listen 9000 port'))
-    }catch(err){
-        console.log(err)
-    }
+    // try{
+    //     await sequelize.authenticate()
+    //     await sequelize.sync()
+    //     app.listen(9000, () => console.log('Server listen 9000 port'))
+    // }catch(err){
+    //     console.log(err)
+    // }
+    https.createServer(options, app)
+        .listen(9000, function (req, res) {
+            console.log("Server started at port 9000");
+    });
 }
 start()
